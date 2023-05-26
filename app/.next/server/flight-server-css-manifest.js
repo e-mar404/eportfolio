61284ccd9b961e680705c92137fdf9a8155cc2c0
e-mar404 <egmarinn@cougarnet.uh.edu>
2023-05-26 15:12:48 -0500

    navbar highlights current page

---
+++ app/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,56 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/emar/Documents/Web Dev/eportfolio/app/page.tsx": [
+      "/Users/emar/Documents/Web Dev/eportfolio/app/page.module.css"
+    ],
+    "/Users/emar/Documents/Web Dev/eportfolio/app/layout.tsx": [
+      "/Users/emar/Documents/Web Dev/eportfolio/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Mulish\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"400\",\"style\":\"normal\"}],\"variableName\":\"mulish\"}",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/globals.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/Navbar.module.css"
+    ],
+    "/Users/emar/Documents/Web Dev/eportfolio/app/about/page.tsx": [
+      "/Users/emar/Documents/Web Dev/eportfolio/app/about/page.module.css"
+    ],
+    "/Users/emar/Documents/Web Dev/eportfolio/app/projects/page.tsx": [
+      "/Users/emar/Documents/Web Dev/eportfolio/app/projects/page.module.css"
+    ],
+    "/Users/emar/Documents/Web Dev/eportfolio/app/contact-me/page.tsx": [
+      "/Users/emar/Documents/Web Dev/eportfolio/app/contact-me/page.module.css"
+    ],
+    "/Users/emar/Documents/Web Dev/eportfolio/app/home/page.tsx": [
+      "/Users/emar/Documents/Web Dev/eportfolio/app/home/page.module.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/emar/Documents/Web Dev/eportfolio/app/page": [
+      "/Users/emar/Documents/Web Dev/eportfolio/app/page.module.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/globals.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Mulish\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"400\",\"style\":\"normal\"}],\"variableName\":\"mulish\"}",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/Navbar.module.css"
+    ],
+    "/Users/emar/Documents/Web Dev/eportfolio/app/about/page": [
+      "/Users/emar/Documents/Web Dev/eportfolio/app/about/page.module.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/globals.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Mulish\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"400\",\"style\":\"normal\"}],\"variableName\":\"mulish\"}",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/Navbar.module.css"
+    ],
+    "/Users/emar/Documents/Web Dev/eportfolio/app/projects/page": [
+      "/Users/emar/Documents/Web Dev/eportfolio/app/projects/page.module.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/globals.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Mulish\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"400\",\"style\":\"normal\"}],\"variableName\":\"mulish\"}",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/Navbar.module.css"
+    ],
+    "/Users/emar/Documents/Web Dev/eportfolio/app/contact-me/page": [
+      "/Users/emar/Documents/Web Dev/eportfolio/app/contact-me/page.module.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/globals.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/Navbar.module.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Mulish\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"400\",\"style\":\"normal\"}],\"variableName\":\"mulish\"}"
+    ],
+    "/Users/emar/Documents/Web Dev/eportfolio/app/home/page": [
+      "/Users/emar/Documents/Web Dev/eportfolio/app/home/page.module.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/globals.css",
+      "/Users/emar/Documents/Web Dev/eportfolio/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Mulish\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"400\",\"style\":\"normal\"}],\"variableName\":\"mulish\"}",
+      "/Users/emar/Documents/Web Dev/eportfolio/app/Navbar.module.css"
+    ]
+  }
+}
--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,13 +1,18 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
+    const path = usePathname();
+
     return(
         <div className={styles.background}>
             <div className={styles.links_container}>
-                <a href='/'>Home</a>
-                <a href='/about'>About</a>
-                <a href='/projects'>Projects</a>
-                <a href='/contact-me'>Contact Me</a>
+                <a href='/' className={path == '/' ? styles.active_link : styles.inactive_link}>Home</a>
+                <a href='/about' className={path.startsWith('/about') ? styles.active_link : styles.inactive_link}>About</a>
+                <a href='/projects' className={path.startsWith('/projects') ? styles.active_link : styles.inactive_link}>Projects</a>
+                <a href='/contact-me' className={path.startsWith('/contact-me') ? styles.active_link : styles.inactive_link}>Contact Me</a>
             </div>
         </div>
     );
