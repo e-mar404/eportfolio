a414f568821f3142d0f96c72b4aa4f3b9fdab2e5
e-mar404 <egmarinn@cougarnet.uh.edu>
2023-09-07 18:41:52 -0500

    journal tab and contact me tab

--- app/Hamburger.tsx
+++ app/Hamburger.tsx
@@ -27,7 +27,7 @@ export default function Hamburger(){
                 <div className={styles.links_container}>
                     <a href='/' className={path == '/' ? styles.active_link : styles.inactive_link}>Home</a>
                     <a href='/about' className={path.startsWith('/about') ? styles.active_link : styles.inactive_link}>About</a>
-                    <a href='/projects' className={path.startsWith('/projects') ? styles.active_link : styles.inactive_link}>Projects</a>
+                    <a href='/journal' className={path.startsWith('/journal') ? styles.active_link : styles.inactive_link}>Projects</a>
                     <a href='/contact-me' className={path.startsWith('/contact-me') ? styles.active_link : styles.inactive_link}>Contact Me</a>
                 </div>
                 <div className={styles.contacts_container}>
--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -1,9 +1,81 @@
+'use client';
+
+import { FormEvent, useState } from 'react';
 import styles from './page.module.css'
 
 export default function Home() {
+    
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [popup, setPopup] = useState(false);
+
+    const submit = (e: FormEvent) => {
+        e.preventDefault();
+
+        console.log('Name: ', name);
+        console.log('Email: ', email);
+        console.log('Message:', message);
+        
+        setPopup(true);
+
+        setName('');
+        setEmail('');
+        setMessage('');
+
+        setTimeout(() => {
+            setPopup(false);
+        }, 5000);
+    };
+
+    
     return(
         <>
             <h1 className={styles.title}>Contact Me Page</h1>
+            <form onSubmit={submit}>
+                <div className={styles.inputContainer}>
+                    <label className={styles.element} htmlFor="name">Name:</label>
+                    <input
+                        className={styles.nameInput}
+                        type="text"
+                        id="name"
+                        autoComplete='true'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label className={styles.element}  htmlFor="email">Email:</label>
+                    <input
+                        className={styles.emailInput}
+                        type="email"
+                        id="email"
+                        autoComplete='true'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label className={styles.element} htmlFor="message">Message:</label>
+                    <textarea
+                        className={styles.messageInput}
+                        id="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        required
+                    />
+                </div>
+                <button className={styles.button} type="submit">Submit</button>
+            </form>
+
+            {popup && (
+                <div className={styles.popup}>
+                    <p>Message Sent Successfully</p>
+                    <button onClick={() => setPopup(false)}>Close</button>
+                </div>
+            )}
         </>
     );
 }
