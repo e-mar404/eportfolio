567cc187a1133423bdd3d41e9a3713433f4a1c43
e-mar404 <egmarinn@cougarnet.uh.edu>
2023-09-21 19:20:54 -0500

    map over projects in career page

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -1,9 +1,24 @@
 import styles from './page.module.css'
+import { Portfolio } from './Portfolio';
 
 export default function Home() {
+    const Projects = [
+        {
+            title: 'Project 1',
+            description: 'Description of project 1',
+            date: 'Sep 01 - Nov 10',
+        },
+        {
+            title: 'Project 2',
+            description: 'Description of project 2',
+            date: '',
+        },
+    ]
+
     return(
         <>
             <h1 className={styles.title}>Career Page</h1>
+            <Portfolio projects={Projects} />
         </>
     );
 }
