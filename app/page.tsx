fd7f07150c6391ca3d2ce8706c69e2d366333146
e-mar404 <egmarinn@cougarnet.uh.edu>
2023-09-23 00:45:30 -0500

    simple home/about page to start getting out there

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import Image from 'next/image';
 import styles from './page.module.css'
+import fun_headshot from './fun_headshot.jpg'
 
 export default function Home() {
   return (
     <main className={styles.background}>
-      <h1 className={styles.title}>Hello, my name is Emilio Marin</h1>
-      <h2 className={styles.title}>I am a software developer and student at the University of Houston</h2>
+      <div className={styles.profile_container}>
+        <Image className={styles.profile_pic} src={fun_headshot} alt='Profile Picture'/>
+          <p className={styles.title}>Hello, my name is Emilio Marin</p>
+          <p className={styles.text}>I am an aspiring software developer studying Computer Science at the University of Houston. I have a strong commitment to continous self-improvement and genuine enthusiasm for acquiring fresh knowledge and skills.</p>
+      </div>
     </main>
   )
 }
